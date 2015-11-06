199925a963c57f1fc9ff8b06455e917a5bea844a
Roopa1593 <roopa1593@gmail.com>
2015-11-06 16:32:15 +0530

    Helloworld modified and fact file edited

--- fact.c
+++ fact.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+int main( )
 {
-	int num,res;
-		clrscr();
-			scanf("Enter the Number %d",&num);
-				res=fact(n);
-					printf("Factorial : %d",res);
-}						getch();
-fact(n)
-{
-	if num = 0 ||1 
-			return 1;
-				else 
-					
-							return fact(n-1);
-}
+        int num,res;
+	                clrscr();
+			                        scanf("Enter the Number %d",&num);
+						                                res=fact(n);
+										                                        printf("Factorial : %d",res);
+															                                        return 0;
+																				} 
+																				
+																				getch();
+																				
+																				fact(n)
+																				{
+																				        if num = 0 ||1
+																					                        return 1;
+																								                                else
+
+																												                                                        return fact(n-1);
+																																			}
